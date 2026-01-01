90fa42d221861fd1be826dea909de5c23d05fed5
d33pto3 <rakinfrhn@gmail.com>
2026-01-01 12:31:09 +0600

    feat: Implement sliding drawer functionality by centralizing drawer state in App and dynamically positioning Navbar and Pahar components.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,24 @@ import React from "react";
 
 type Props = {
   children: React.ReactElement;
+  drawerOpen: boolean;
 };
 
-const Navbar = (props: Props) => {
+const Navbar = ({ children, drawerOpen }: Props) => {
   return (
     <>
-      <div className="absolute w-full z-1 backdrop-blur-lg px-16 py-2">
+      <div
+        className={`fixed top-0 z-1 backdrop-blur-lg px-16 py-2 transition-all duration-300 right-0 ${
+          drawerOpen
+            ? "left-[85vw] sm:left-[70vw] md:left-[380px] lg:left-[440px] xl:left-[500px]"
+            : "left-0"
+        }`}
+      >
         <div>
           <img src="/images/icon/mountain.svg" className="h-14 w-14 invert" />
         </div>
       </div>
-
-      {props.children}
+      {children}
     </>
   );
 };
--- src/components/Pahar.tsx
+++ src/components/Pahar.tsx
@@ -6,16 +6,21 @@ import Drawer, { type Mountain } from "./Drawer";
 import Title from "./pahar/Title";
 import axios from "axios";
 
-const Pahar = () => {
+type Props = {
+  drawerOpen: boolean;
+  toggleDrawer: () => void;
+};
+
+const Pahar = ({ drawerOpen, toggleDrawer }: Props) => {
   const sliderRef = useRef<Slider | null>(null);
-  const [drawerOpen, setDrawerOpen] = useState(false);
+
   const [mountains, setMountains] = useState<Mountain[] | []>([]);
   const [selectedMountain, setSelectedMountain] = useState<Mountain | null>(
     null
   );
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -51,23 +56,31 @@ const Pahar = () => {
     getMountains();
   }, []);
 
-  const toggleDrawer = () => setDrawerOpen((prev) => !prev);
+
 
   return (
     <div className="h-screen w-screen overflow-hidden relative">
-      <Slider ref={sliderRef} {...settings} className="h-screen">
-        {mountains?.map((mountain: Mountain) => (
-          <div key={mountain.key} className="relative w-screen h-screen">
-            <div
-              className="w-full h-full bg-cover bg-center cursor-pointer"
-              style={{ backgroundImage: `url(${mountain.mountain_img})` }}
-              onClick={toggleDrawer}
-            />
-          </div>
-        ))}
-      </Slider>
+      <div
+        className={`h-full transition-all duration-300 relative ${
+          drawerOpen
+            ? "ml-[85vw] sm:ml-[70vw] md:ml-[380px] lg:ml-[440px] xl:ml-[500px]"
+            : "ml-0"
+        }`}
+      >
+        <Slider ref={sliderRef} {...settings} className="h-screen">
+          {mountains?.map((mountain: Mountain) => (
+            <div key={mountain.key} className="relative w-full h-screen">
+              <div
+                className="w-full h-full bg-cover bg-center cursor-pointer"
+                style={{ backgroundImage: `url(${mountain.mountain_img})` }}
+                onClick={toggleDrawer}
+              />
+            </div>
+          ))}
+        </Slider>
 
-      <Title key={selectedMountain?.key} title={selectedMountain?.key} />
+        <Title key={selectedMountain?.key} title={selectedMountain?.key} />
+      </div>
 
       <Drawer
         mountain={selectedMountain}
